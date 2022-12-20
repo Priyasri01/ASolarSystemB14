9b267b1fc6f8682b7d5a3a8e9a1c2378ff121a4b
priyasri01 <Priyasri.april14@gmail.com>
2022-12-20 15:29:34 -0500

    The basic of solor system are ready and deploy

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+       SolorSystem xx = new SolorSystem();
+       xx.moon=1;
+       xx.sun=1;
+       xx.planet=2;
+    }
+}
---
+++ src/SolorSystem.java
@@ -0,0 +1,5 @@
+public class SolorSystem {
+    int sun;
+    int moon;
+    int planet;
+}
